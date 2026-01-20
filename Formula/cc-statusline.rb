f7c6765935d0d6f4372906da259c1de40a636d97
Ali Karbassi <ali.karbassi@bebraven.org>
2026-01-20 09:58:19 -0600

    Use version interpolation in URLs

--- Formula/cc-statusline.rb
+++ Formula/cc-statusline.rb
@@ -6,22 +6,22 @@ class CcStatusline < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/karbassi/cc-status-line/releases/download/v0.1.1/cc-statusline-macos-arm64.tar.gz"
+      url "https://github.com/karbassi/cc-status-line/releases/download/v#{version}/cc-statusline-macos-arm64.tar.gz"
       sha256 "b34723f9567b623505efa41c54d0341ca63f014a7b211746bb1a2a788436b313"
     end
     on_intel do
-      url "https://github.com/karbassi/cc-status-line/releases/download/v0.1.1/cc-statusline-macos-x86_64.tar.gz"
+      url "https://github.com/karbassi/cc-status-line/releases/download/v#{version}/cc-statusline-macos-x86_64.tar.gz"
       sha256 "6b4d745388e54fc2506b35aeb17cabfb80a47389be05021c20f2eabecc36643f"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/karbassi/cc-status-line/releases/download/v0.1.1/cc-statusline-linux-arm64.tar.gz"
+      url "https://github.com/karbassi/cc-status-line/releases/download/v#{version}/cc-statusline-linux-arm64.tar.gz"
       sha256 "9d4776423b19f05a31f4e0a91990260e788cb413b077336a4be318fbd5740ab0"
     end
     on_intel do
-      url "https://github.com/karbassi/cc-status-line/releases/download/v0.1.1/cc-statusline-linux-x86_64.tar.gz"
+      url "https://github.com/karbassi/cc-status-line/releases/download/v#{version}/cc-statusline-linux-x86_64.tar.gz"
       sha256 "622d04511d3e9f68f649f8e10220644a3ebe8160778e95c82cd679c25669bca8"
     end
   end
